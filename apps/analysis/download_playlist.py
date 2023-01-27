a35247592ad4739ca408135eec6354cf2d7abb96
tozastation <tozawa@3-shake.com>
2023-01-27 17:06:54 +0900

    initial commit

--- apps/analysis/download_playlist.py
+++ apps/analysis/download_playlist.py
@@ -13,7 +13,7 @@ spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
 results = spotify.current_user_playlists()
 
-TARGET_PLAYLIST_NAME="TARAKO PARTY MIX"
+TARGET_PLAYLIST_NAME = "TARAKO PARTY MIX"
 
 target_playlist_uri: str = None
 
@@ -33,30 +33,47 @@ with open("./files/target_playlist.json", "w") as outfile:
 
 tracks: [Track] = []
 
-for idx, item in enumerate(target_playlist['tracks']['items']):
-    track_id: str = item['track']['id']
-    track_uri: str = item['track']['uri']
-    track_artists: [Artist] = Artist.schema().loads(
-        json.dumps(item['track']['album']['artists']),
-        many=True,
-        unknown=EXCLUDE,
-    )
-    track_name: str = item['track']['name']
-    track_link: str = item['track']['external_urls']['spotify']
-    image_x64 = item['track']['album']['images'][2]
-    tracks.append(Track(
-        id=track_id,
-        uri=track_uri,
-        artists=track_artists,
-        name=track_name,
-        link=track_link
-    ))
+playlist_items_offset = 0
+playlist_item_count = 0
+playlist_item_limit = 0
+while True:
+    playlist_items_result = spotify.playlist_items(playlist_id=target_playlist_uri, limit=50,
+                                                   offset=playlist_items_offset)
+    playlist_item_count += len(playlist_items_result['items'])
+    print('playlist item count => {}'.format(playlist_item_count))
+    print('playlist item offset => {}'.format(playlist_items_offset))
+    print('playlist_items_result len => {}'.format(len(playlist_items_result['items'])))
+    for idx, item in enumerate(playlist_items_result['items']):
+        track_id: str = item['track']['id']
+        track_uri: str = item['track']['uri']
+        track_artists: [Artist] = Artist.schema().loads(
+            json.dumps(item['track']['album']['artists']),
+            many=True,
+            unknown=EXCLUDE,
+        )
+        track_name: str = item['track']['name']
+        track_link: str = item['track']['external_urls']['spotify']
+        image_x64 = item['track']['album']['images'][2]
+        tracks.append(Track(
+            id=track_id,
+            uri=track_uri,
+            artists=track_artists,
+            name=track_name,
+            link=track_link
+        ))
+    # オフセットを求めたい
+    # オフセット = 50×N
+    # 全体 >
+    if playlist_items_result['total'] > playlist_item_count:
+        remain_item_count = playlist_items_result['total'] - playlist_item_count
+        playlist_items_offset += 50
+    else:
+        break
 
-#print(tracks)
-print(Track.schema().dumps(tracks, many=True))
+print(len(tracks))
+# print(Track.schema().dumps(tracks, many=True))
 
 with open("./files/target_playlist_track.json", "w") as outfile:
     outfile.write(Track.schema().dumps(tracks, many=True, indent=2))
 
-
-#print(tracks)
+# print(tracks)
